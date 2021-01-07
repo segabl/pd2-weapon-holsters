dc58d08599f5edfaeb493106b431774af651b792
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-01-07 13:48:01 +0100

    Remove NPC weapon showing up on back
    Sometimes the game will display the NPC pistol instead of the actual weapon.
    Disabled NPC weapons from showing up on the player, still need to find out why it happens.

--- mod.lua
+++ mod.lua
@@ -1,99 +1,102 @@
 WeaponHolsters = {
-  -- Align places for specific hold types (default is back)
-  hold_align_names = {
-    pistol = "hips_right",
-    akimbo_pistol = "hips_right",
-    bow = "bow"
-  },
-  -- Align places for specific weapons
-  align_overrides = {
-    frankish_crew = "crossbow",
-    arblast_crew = "crossbow",
-    m134_crew = "minigun"
-  }
+	-- Align places for specific hold types (default is back)
+	hold_align_names = {
+		pistol = "hips_right",
+		akimbo_pistol = "hips_right",
+		bow = "bow"
+	},
+	-- Align places for specific weapons
+	align_overrides = {
+		frankish_crew = "crossbow",
+		arblast_crew = "crossbow",
+		m134_crew = "minigun"
+	}
 }
 
 
 if RequiredScript == "lib/units/beings/player/huskplayerinventory" then
 
-  -- Add new align places
-  Hooks:PostHook(HuskPlayerInventory, "init", "weaponholsters_init", function (self)
+	-- Add new align places
+	Hooks:PostHook(HuskPlayerInventory, "init", "weaponholsters_init", function (self)
 
-    self._align_places.back = {
-      on_body = true,
-      obj3d_name = Idstring("BackPack1"),
-      local_rot = Rotation(270, 0, 90),
-      local_pos = Vector3(-15, 5, 10)
-    }
-    self._align_places.hips_right = {
-      on_body = true,
-      obj3d_name = Idstring("Hips"),
-      local_rot = Rotation(170, 0, 0),
-      local_pos = Vector3(-20, 5, 0),
-    }
-    self._align_places.hips_left = {
-      on_body = true,
-      obj3d_name = Idstring("Hips"),
-      local_rot = Rotation(190, 0, 0),
-      local_pos = Vector3(20, 5, 0)
-    }
-    -- Specialized align places
-    self._align_places.bow = {
-      on_body = true,
-      obj3d_name = Idstring("BackPack1"),
-      local_rot = Rotation(90, 0, 80),
-      local_pos = Vector3(-20, 10, 10)
-    }
-    self._align_places.crossbow = {
-      on_body = true,
-      obj3d_name = Idstring("BackPack1"),
-      local_rot = Rotation(0, 0, 0),
-      local_pos = Vector3(-15, -20, 5)
-    }
-    self._align_places.minigun = {
-      on_body = true,
-      obj3d_name = Idstring("BackPack1"),
-      local_rot = Rotation(270, 0, 90),
-      local_pos = Vector3(-40, 0, 5)
-    }
-    -- Where to align the scond gun of akimbo weapons
-    self._align_places.right_hand.second_gun = self._align_places.left_hand
-    self._align_places.hips_right.second_gun = self._align_places.hips_left
+		self._align_places.back = {
+			on_body = true,
+			obj3d_name = Idstring("BackPack1"),
+			local_rot = Rotation(270, 0, 90),
+			local_pos = Vector3(-15, 5, 10)
+		}
+		self._align_places.hips_right = {
+			on_body = true,
+			obj3d_name = Idstring("Hips"),
+			local_rot = Rotation(170, 0, 0),
+			local_pos = Vector3(-20, 5, 0),
+		}
+		self._align_places.hips_left = {
+			on_body = true,
+			obj3d_name = Idstring("Hips"),
+			local_rot = Rotation(190, 0, 0),
+			local_pos = Vector3(20, 5, 0)
+		}
+		-- Specialized align places
+		self._align_places.bow = {
+			on_body = true,
+			obj3d_name = Idstring("BackPack1"),
+			local_rot = Rotation(90, 0, 80),
+			local_pos = Vector3(-20, 10, 10)
+		}
+		self._align_places.crossbow = {
+			on_body = true,
+			obj3d_name = Idstring("BackPack1"),
+			local_rot = Rotation(0, 0, 0),
+			local_pos = Vector3(-15, -20, 5)
+		}
+		self._align_places.minigun = {
+			on_body = true,
+			obj3d_name = Idstring("BackPack1"),
+			local_rot = Rotation(270, 0, 90),
+			local_pos = Vector3(-40, 0, 5)
+		}
+		-- Where to align the scond gun of akimbo weapons
+		self._align_places.right_hand.second_gun = self._align_places.left_hand
+		self._align_places.hips_right.second_gun = self._align_places.hips_left
 
-  end)
+	end)
 
-  -- Turn off gadgets when holstering weapons
-  Hooks:PostHook(HuskPlayerInventory, "_place_selection", "weaponholsters_place_selection", function (self, selection_index, is_equip)
+	-- Turn off gadgets when holstering weapons
+	Hooks:PostHook(HuskPlayerInventory, "_place_selection", "weaponholsters_place_selection", function (self, selection_index, is_equip)
 
-    local unit = not is_equip and self._available_selections[selection_index].unit
-    if unit and unit:base().gadget_off then
-      unit:base():gadget_off()
-    end
+		local unit = not is_equip and self._available_selections[selection_index].unit
+		if unit and unit:base().gadget_off then
+			unit:base():gadget_off()
+		end
 
-  end)
+	end)
 
-  -- Adjust weapon position/rotation and account for akimbo
-  Hooks:PostHook(HuskPlayerInventory, "_link_weapon", "weaponholsters_link_weapon", function (self, unit, align_place)
+	-- Adjust weapon position/rotation and account for akimbo
+	Hooks:PostHook(HuskPlayerInventory, "_link_weapon", "weaponholsters_link_weapon", function (self, unit, align_place)
 
-    unit:set_local_position(align_place.local_pos or Vector3())
-    unit:set_local_rotation(align_place.local_rot or Rotation())
-    if unit:base()._second_gun then
-      self:_link_weapon(unit:base()._second_gun, align_place.second_gun or align_place)
-    end
+		unit:set_local_position(align_place.local_pos or Vector3())
+		unit:set_local_rotation(align_place.local_rot or Rotation())
+		if unit:base()._second_gun then
+			self:_link_weapon(unit:base()._second_gun, align_place.second_gun or align_place)
+		end
 
-  end)
+	end)
 
 end
 
 
 if RequiredScript == "lib/units/weapons/newnpcraycastweaponbase" then
 
-  -- Check align place overrides
-  Hooks:PostHook(NewNPCRaycastWeaponBase, "_create_use_setups", "weaponholsters_create_use_setups", function (self)
+	-- Check align place overrides
+	Hooks:PostHook(NewNPCRaycastWeaponBase, "_create_use_setups", "weaponholsters_create_use_setups", function (self)
+		local new_align_place = WeaponHolsters.align_overrides[self._name_id] or WeaponHolsters.hold_align_names[self:weapon_tweak_data().hold]
+		self._use_data.player.unequip.align_place = new_align_place or self._use_data.player.unequip.align_place
+	end)
 
-    local new_align_place = WeaponHolsters.align_overrides[self._name_id] or WeaponHolsters.hold_align_names[self:weapon_tweak_data().hold]
-    self._use_data.player.unequip.align_place = new_align_place or self._use_data.player.unequip.align_place
-
-  end)
+	-- Remove back align place for non player weapons
+	Hooks:PostHook(NPCRaycastWeaponBase, "_create_use_setups", "weaponholsters_create_use_setups", function (self)
+		self._use_data.player.unequip.align_place = nil
+	end)
 
 end
